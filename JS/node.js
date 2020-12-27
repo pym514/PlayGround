6684510bfe5af866cede09e1e180ad21360357c0
yongman park <yongman514@gmail.com>
2020-12-27 18:37:16 +0900

    addAndDeleteRow

---
+++ JS/node.js
@@ -0,0 +1,11 @@
+// var http = requre('http');
+// http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-type': 'text/plain'});
+//     res.write('hello world\n');
+//     res.end();
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337/');
+
+function myFunction(){
+      var name = Broswer.msgBox('Hello world');
+}
